0f506b80901bc54a0e26ef968d94607369290982
Anshuman Tiwari <anshuman21153045@akgec.ac.in>
2024-02-25 16:29:58 +0530

    signin api made

--- Controller/userCtrl.js
+++ Controller/userCtrl.js
@@ -1,6 +1,7 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient()
 const bcrypt=require("bcrypt");
+const jwt=require("jsonwebtoken");
 const userCtrl={
     signup:async(req,res)=>{
         try{
@@ -21,13 +22,16 @@ const userCtrl={
                       email,
                       password:passwordHash
                     },
-                })
+                });
+                console.log(user);
+                const accesstoken=createAccessToken({id:user.id});
+
                 
-                // console.log(user);
 
                 res.status(200).json({
                     success: true,
                     msg: "Signup successful !",
+                    accesstoken
                 });
 
             }
@@ -39,7 +43,40 @@ const userCtrl={
             res.status(400).json({success:false,msg:error.message});
             console.log(error);
         }
+    },
+    signin:async(req,res)=>{
+        try{
+            let {email,password}=req.body;
+            email=email.toLowerCase();
+
+            const getUser= await prisma.user.findUnique({
+                where:{
+                    email:email
+                }
+            })
+            const result = await bcrypt.compare(password, getUser.password);
+            if(!result)throw new Error("Password did not match !")
+            const accesstoken=createAccessToken({id:getUser.id});
+            res.status(200).json({
+                success: true,
+                msg: "Sigin successful !",
+                accesstoken
+            });
+
+            // if(!getUser)throw new Error("No user found !");
+            // const result=await bcrypt.compare(password,getUser.password)
+        }
+        catch(error){
+            res.status(400).json({success:false,msg:error.message});
+            console.log(error);
+        }
     }
+
 }
+
+const createAccessToken=(user)=>{
+    return jwt.sign(user,process.env.ACCESS_TOKEN_SECRET);
+}
+
 
 module.exports=userCtrl;
--- Router/userRouter.js
+++ Router/userRouter.js
@@ -1,4 +1,8 @@
 const router=require("express").Router();
 const userCtrl=require('../Controller/userCtrl');
+
+
 router.post("/signup",userCtrl.signup);
+router.post("/signin",userCtrl.signin);
+
 module.exports=router;
